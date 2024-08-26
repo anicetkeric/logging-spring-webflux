2728a8fe98de2426219dc19301f8120867f0b9c9
aek <anicetkeric@outlook.com>
2024-08-26 19:39:25 +0200

    update serializer

--- src/main/java/com/bootlabs/logging/config/LogbookConfiguration.java
+++ src/main/java/com/bootlabs/logging/config/LogbookConfiguration.java
@@ -1,13 +1,18 @@
 package com.bootlabs.logging.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.WebFilter;
+import org.zalando.logbook.HttpLogFormatter;
 import org.zalando.logbook.Logbook;
+import org.zalando.logbook.Sink;
 import org.zalando.logbook.autoconfigure.webflux.LogbookWebFluxAutoConfiguration;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+import org.zalando.logbook.logstash.LogstashLogbackSink;
 import org.zalando.logbook.spring.webflux.LogbookExchangeFilterFunction;
 import org.zalando.logbook.spring.webflux.LogbookWebFilter;
 
@@ -31,4 +36,11 @@ public class LogbookConfiguration {
                 .filter(logbookClientExchangeFunction)
                 .build();
     }
+
+    @Bean
+    public Sink sink(ObjectMapper objectMapper) {
+        HttpLogFormatter formatter = new JsonHttpLogFormatter(objectMapper);
+        return new LogstashLogbackSink(formatter);
+    }
+
 }
